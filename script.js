c61aa8edd136ce64fddf5522a618294f333ca227
Medmly20208 <moulaymohamed856@gmail.com>
2022-11-18 20:17:08 +0100

    finishing project

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
+// get main tag
+const productsContainer = document.getElementById("productsContainer");
+const ProductCardSection = document.createElement("section");
+
+ProductCardSection.classList.add("productCard");
+
+// product Card Structure
+
+//
+const getProductCardStructure = (productName, productPrice, productImage) => {
+  const ProductCardSection = document.createElement("section");
+  ProductCardSection.classList.add("productCard");
+
+  ProductCardSection.innerHTML = `
+  
+            <div class="imageContainer">
+               <img src=${productImage} />
+            </div>
+            <div class="contentContainer">
+
+            
+               <div>
+                 <h1>${productName}</h1>
+              
+                 </div>
+                <div class="price">
+                   <button>Buy Now</button>
+                   <h2>$${productPrice}</h2>
+                </div>
+            </div>
+       
+      
+  
+  `;
+
+  return ProductCardSection;
+};
+
 // get products data
 const getProductData = async () => {
   const data = await fetch("http://127.0.0.1:5000/data");
-  const res = await data.json();
+  const productsList = await data.json();
 
-  console.log(res);
+  for (let product of productsList) {
+    productsContainer.appendChild(
+      getProductCardStructure(
+        product.productName,
+        product.productPrice,
+        product.image
+      )
+    );
+  }
 };
 
 getProductData();
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const http = require("http");
 const fs = require("fs");
 
+//get a string
 const ProductsData = fs.readFileSync("./data/products.json");
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+  //console.log(url.parse(req.url));
 
+  // overfiew path
   if (pathName === "/data") {
-    fs.readFile(`./data/products.json`, "utf-8", (err, data) => {
-      res.writeHead(200, {
-        "Access-Control-Allow-Origin": "*",
-        "Content-type": "application/json",
-      });
-      res.end(data);
+    // slove cors policy bug and specify type of response data
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "*",
+      "Content-type": "application/json",
     });
-  } else {
+    res.end(ProductsData);
+  }
+
+  // not found page
+  else {
     res.end("not found brudda");
   }
 });
