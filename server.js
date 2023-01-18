56eaff31aa86df5bc72c599f6852d2044ec9b48f
Mohamed Moulay <69359301+Medmly20208@users.noreply.github.com>
2023-01-18 11:35:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const PORT = process.env.PORT || 3030;
 
 const ProductsData = fs.readFileSync("./data/products.json");
 
@@ -23,6 +24,6 @@ const server = http.createServer((req, res) => {
 });
 
 // listen to server
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log("listening on port 5000");
 });
